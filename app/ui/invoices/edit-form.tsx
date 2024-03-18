322e7ffaa7f0274c9f6aa9b31f8928f3b47e4bba
MK884 <marchantkhalid7810@gmail.com>
2024-03-18 20:43:12 +0530

    dashboard-invoice

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -1,5 +1,4 @@
 'use client';
-
 import { CustomerField, InvoiceForm } from '@/app/lib/definitions';
 import {
   CheckIcon,
@@ -7,6 +6,7 @@ import {
   CurrencyDollarIcon,
   UserCircleIcon,
 } from '@heroicons/react/24/outline';
+import { updateInvoice } from '@/app/lib/actions';
 import Link from 'next/link';
 import { Button } from '@/app/ui/button';
 
@@ -17,8 +17,10 @@ export default function EditInvoiceForm({
   invoice: InvoiceForm;
   customers: CustomerField[];
 }) {
+  const updateInvoiceWithId = updateInvoice.bind(null, invoice.id);
+
   return (
-    <form>
+    <form action={updateInvoiceWithId}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
